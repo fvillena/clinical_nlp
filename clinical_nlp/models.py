b316b6dc1d86ae1058e179d16506f81cddc444cc
Fabián Villena <fvillena@proton.me>
2023-08-02 14:23:24 -0400

    icl ner

--- clinical_nlp/models.py
+++ clinical_nlp/models.py
@@ -1,8 +1,10 @@
 import requests
 import json
 from unidecode import unidecode
+import re
 # logger
 import logging
+
 logger = logging.getLogger(__name__)
 
 
@@ -12,16 +14,6 @@ class IclModel:
         self.max_tokens = max_tokens
         self.stop = stop
         self.max_retries = max_retries
-
-    def contextualize(self, system_message, classes, user_template, retry_message):
-        classes_string = ""
-        for c in classes[:-1]:
-            classes_string += c + ", "
-        classes_string += f"y {classes[-1]}"
-        self.system_message = system_message.replace("<classes>", classes_string)
-        self.classes = classes
-        self.user_template = user_template
-        self.retry_message = retry_message
 
     def query_model(self, messages):
         payload = json.dumps(
@@ -38,9 +30,22 @@ class IclModel:
         completion = response.json()["choices"][0]["message"]["content"].strip()
         return completion
 
+
+class IclClassifier(IclModel):
+    def contextualize(self, system_message, classes, user_template, retry_message):
+        classes_string = ""
+        for c in classes[:-1]:
+            classes_string += c + ", "
+        classes_string += f"y {classes[-1]}"
+        self.system_message = system_message.replace("<classes>", classes_string)
+        self.classes = classes
+        self.user_template = user_template
+        self.retry_message = retry_message
+
     def predict(self, x):
         def preprocess(sentence):
             return unidecode(sentence.strip().lower())
+
         def label(completion):
             y = None
             for c in self.classes:
@@ -48,6 +53,7 @@ class IclModel:
                     y = c.upper()
                     break
             return y
+
         messages = [
             {"content": f"{self.system_message}", "role": "system"},
             {
@@ -66,4 +72,50 @@ class IclModel:
                 messages.append({"content": completion, "role": "assistant"})
                 messages.append({"content": self.retry_message, "role": "user"})
         return y
-        
+
+
+class IclNer(IclModel):
+    def contextualize(self, system_message, user_template, entities):
+        keys = list(entities.keys())
+        values = list(entities.values())
+        items = list(entities.items())
+        entity_names_string = ""
+        for e in values[:-1]:
+            entity_names_string += e + ", "
+        entity_names_string += f"y {values[-1]}"
+        schema_string = ""
+        for e,d in items[:-1]:
+            schema_string += f'"{e}" para {d}' + ", "
+        schema_string += f'y "{items[-1][0]}" para {items[-1][1]}'
+        self.system_message = system_message.replace("<entities>", entity_names_string).replace("<schema>", schema_string)
+        self.user_template = user_template
+    
+    def predict(self, x):
+        def get_json(completion):
+            try:
+                json = re.search(r".*({.*}).*", completion, flags=re.DOTALL).group(1).strip()
+            except:
+                raise Exception(f"No JSON was found on your answer.")
+            return re.sub("(\w+):", r'"\1":',  json)
+        messages = [
+            {"content": f"{self.system_message}", "role": "system"},
+            {
+                "content": f"{self.user_template}".replace("<x>", x),
+                "role": "user",
+            },
+        ]
+        completion = self.query_model(messages)
+        logger.info(f"Completion: {completion}")
+        try:
+            y = json.loads(get_json(completion))
+        except Exception as e:
+            logger.warning(e)
+            messages.append({"content": completion, "role": "assistant"})
+            messages.append({"content": f"No puedo decodificar tu json porque tiene este error: {e}", "role": "user"})
+            try:
+                completion = self.query_model(messages)
+                y = json.loads(get_json(completion))
+            except:
+                logger.warning(f"JSON decoding error: {completion}")
+                y = None
+        return y
--- predict.py
+++ predict.py
@@ -9,7 +9,8 @@ from clinical_nlp.arguments import (
     PipelineArguments,
     PredictionArguments,
 )
-from clinical_nlp.models import IclModel
+from clinical_nlp.models import IclClassifier, IclNer
+import re
 
 if __name__ == "__main__":
     parser = HfArgumentParser(
@@ -30,12 +31,12 @@ if __name__ == "__main__":
     if model_args.task == "text-classification":
         if "http" in model_args.model_name_or_path:
             classes = list(set(test_data["label"]))
-            model = IclModel(model_args.model_name_or_path)
+            model = IclClassifier(model_args.model_name_or_path)
             model.contextualize(
                 system_message="Eres un asistente serio que sólo da respuestas precisas y concisas que recibirá diagnósticos en Español y deberás sólo responder con el nombre de la especialidad en Español a la cual debe enviarse el diagnóstico. Las especialidades disponibles son: <classes>.",
                 user_template='¿A qué especialidad debo enviar el diagnóstico "<x>"?.',
                 classes=classes,
-                retry_message="No encuentro en tu mensaje ninguna de las especialidades de la lista de especialidades disponibles. Por favor, intenta nuevamente."
+                retry_message="No encuentro en tu mensaje ninguna de las especialidades de la lista de especialidades disponibles. Por favor, intenta nuevamente.",
             )
             test_data = test_data.map(
                 lambda x: {"prediction": model.predict(x["text"])}, num_proc=12
@@ -60,116 +61,189 @@ if __name__ == "__main__":
             for t, p in zip(true, predicted):
                 f.write(f"{t}\t{p}\n")
     elif model_args.task == "ner":
-        from transformers import (
-            AutoTokenizer,
-            TokenClassificationPipeline,
-            AutoModelForTokenClassification,
-        )
-
-        def tokenize_and_align_labels(examples):
-            tokenized_inputs = tokenizer(
-                examples["tokens"], truncation=True, is_split_into_words=True
+        label_list = data["train"].features[f"ner_tags"].feature.names
+        if "http" in model_args.model_name_or_path:
+            model = IclNer(model_args.model_name_or_path, max_tokens=2048, stop=["###"])
+            model.contextualize(
+                system_message="Eres un detector de menciones de entidades médicas en Español que debe extraer las menciones de <entities> desde los textos que se te entreguen. Formatea la respuesta en json. Sólo responde con el json y nunca uses saltos de línea. Las llaves del json es: <schema>. El valor de cada llave es la lista de menciones para esa entidad.",
+                user_template="<x>",
+                entities={
+                    "disease": "Enfermedad",
+                    "medication": "Medicamento",
+                    "abbreviation": "Abreviatura",
+                    "body_part": "Parte del cuerpo",
+                    "family_member": "Miembro de la familia",
+                    "laboratory_or_test_result": "Resultado de laboratorio o test",
+                    "clinical_finding": "Hallazgo clínico",
+                    "diagnostic_procedure": "Procedimiento diagnóstico",
+                    "laboratory_procedure": "Procedimiento de laboratorio",
+                    "therapeutic_procedure": "Procedimiento terapéutico",
+                },
+            )
+            test_data = test_data.map(
+                lambda x: {"prediction": model.predict(" ".join(x["tokens"]))}
+            )
+            def get_ner_tags(d, tokens):
+                string = " ".join(tokens).lower()
+                idxs = []
+                i = 0
+                tags = ["O"]*len(tokens)
+                for s in string:
+                    if s == " ":
+                        i += 1
+                        idxs.append(-100)
+                    else:
+                        idxs.append(i)
+                if d:
+                    for entity, mentions in d.items():
+                        if mentions:
+                            for mention in mentions:
+                                try:
+                                    for match in re.finditer(mention.lower(), string):
+                                        start = match.start()
+                                        end = match.end()
+                                        start_token_idx = idxs[start]
+                                        end_token_idx = idxs[end-1]
+                                        for j,k in enumerate(range(start_token_idx, end_token_idx+1)):
+                                            if j == 0:
+                                                tags[k] = f"B-{entity}"
+                                            else:
+                                                tags[k] = f"I-{entity}"
+                                except:
+                                    continue
+                return tags
+            sentences = test_data["tokens"]
+            true = list(map(lambda x: [label_list[z].capitalize() for z in x], test_data["ner_tags"]))
+            predicted = list(map(lambda x: [l.capitalize() for l in get_ner_tags(x[1], x[0])], zip(test_data["tokens"], test_data["prediction"])))
+        else:
+            from transformers import (
+                AutoTokenizer,
+                TokenClassificationPipeline,
+                AutoModelForTokenClassification,
             )
 
-            labels = []
-            for i, label in enumerate(examples[f"ner_tags"]):
-                word_ids = tokenized_inputs.word_ids(
-                    batch_index=i
-                )  # Map tokens to their respective word.
-                previous_word_idx = None
-                label_ids = []
-                for word_idx in word_ids:  # Set the special tokens to -100.
-                    if word_idx is None:
-                        label_ids.append(-100)
-                    elif (
-                        word_idx != previous_word_idx
-                    ):  # Only label the first token of a given word.
-                        label_ids.append(label[word_idx])
+            def tokenize_and_align_labels(examples):
+                tokenized_inputs = tokenizer(
+                    examples["tokens"], truncation=True, is_split_into_words=True
+                )
+
+                labels = []
+                for i, label in enumerate(examples[f"ner_tags"]):
+                    word_ids = tokenized_inputs.word_ids(
+                        batch_index=i
+                    )  # Map tokens to their respective word.
+                    previous_word_idx = None
+                    label_ids = []
+                    for word_idx in word_ids:  # Set the special tokens to -100.
+                        if word_idx is None:
+                            label_ids.append(-100)
+                        elif (
+                            word_idx != previous_word_idx
+                        ):  # Only label the first token of a given word.
+                            label_ids.append(label[word_idx])
+                        else:
+                            label_ids.append(-100)
+                        previous_word_idx = word_idx
+                    labels.append(label_ids)
+
+                tokenized_inputs["labels"] = labels
+                return tokenized_inputs
+
+            def process_true_labels(example):
+                true = [
+                    -100 if i == -100 else pipe.model.config.id2label[i]
+                    for i in example
+                ]
+                interim = []
+                for i, piece in enumerate(true):
+                    if i == 0:
+                        interim.append("O")
+                    elif i == len(true) - 1:
+                        interim.append("O")
+                    elif piece != -100:
+                        interim.append(piece)
                     else:
-                        label_ids.append(-100)
-                    previous_word_idx = word_idx
-                labels.append(label_ids)
-
-            tokenized_inputs["labels"] = labels
-            return tokenized_inputs
-        def process_true_labels(example):
-            true = [-100 if i == -100 else pipe.model.config.id2label[i] for i in example]
-            interim = []
-            for i, piece in enumerate(true):
-                if i == 0:
-                    interim.append("O")
-                elif i == len(true)-1:
-                    interim.append("O")
-                elif piece != -100:
-                    interim.append(piece)
-                else:
-                    last_piece = interim[i-1]
-                    interim.append(last_piece if not last_piece.startswith("B-") else last_piece.replace("B-", "I-"))
-            true = interim
-            return true
-        def process_predicted_labels(prediction,sentence):
-            interim = ["O"]*len(sentence)
-            for result in prediction:
-                interim[result["index"]] = result["entity"]
-            predicted = interim
-            return predicted
-        class MyTokenClassificationPipeline(TokenClassificationPipeline):
-            def preprocess(self, sentence, offset_mapping=None, **preprocess_params):
-                tokenizer_params = preprocess_params.pop("tokenizer_params", {})
-                truncation = (
-                    True
-                    if self.tokenizer.model_max_length
-                    and self.tokenizer.model_max_length > 0
-                    else False
-                )
-                inputs = self.tokenizer(
-                    sentence,
-                    return_tensors=self.framework,
-                    truncation=truncation,
-                    return_special_tokens_mask=True,
-                    return_offsets_mapping=self.tokenizer.is_fast,
-                    is_split_into_words=True,
-                    **tokenizer_params,
-                )
-                inputs.pop("overflow_to_sample_mapping", None)
-                num_chunks = len(inputs["input_ids"])
-
-                for i in range(num_chunks):
-                    if self.framework == "tf":
-                        raise Exception(
-                            "TensorFlow pipelines are currently not supported."
+                        last_piece = interim[i - 1]
+                        interim.append(
+                            last_piece
+                            if not last_piece.startswith("B-")
+                            else last_piece.replace("B-", "I-")
                         )
-                    else:
-                        model_inputs = {k: v[i].unsqueeze(0) for k, v in inputs.items()}
-                    if offset_mapping is not None:
-                        model_inputs["offset_mapping"] = offset_mapping
-                    model_inputs["sentence"] = sentence if i == 0 else None
-                    model_inputs["is_last"] = i == num_chunks - 1
+                true = interim
+                return true
 
-                    yield model_inputs
+            def process_predicted_labels(prediction, sentence):
+                interim = ["O"] * len(sentence)
+                for result in prediction:
+                    interim[result["index"]] = result["entity"]
+                predicted = interim
+                return predicted
 
-        tokenizer = AutoTokenizer.from_pretrained(
-            model_args.tokenizer_name, add_prefix_space=True
-        )
-        tokenized_test_data = test_data.map(tokenize_and_align_labels, batched=True)
-        model = AutoModelForTokenClassification.from_pretrained(
-            model_args.model_name_or_path
-        )
-        pipe = MyTokenClassificationPipeline(
-            model=model,
-            tokenizer=tokenizer,
-            device=0,
-            aggregation_strategy="none",
-        )
-        sentences = list(map(pipe.tokenizer.convert_ids_to_tokens, tokenized_test_data["input_ids"]))
-        true = list(map(process_true_labels, tokenized_test_data["labels"]))
-        predictions = pipe(test_data["tokens"])
-        predicted = []
-        for prediction, sentence in zip(predictions, sentences):
-            predicted.append(process_predicted_labels(prediction,sentence))
+            class MyTokenClassificationPipeline(TokenClassificationPipeline):
+                def preprocess(
+                    self, sentence, offset_mapping=None, **preprocess_params
+                ):
+                    tokenizer_params = preprocess_params.pop("tokenizer_params", {})
+                    truncation = (
+                        True
+                        if self.tokenizer.model_max_length
+                        and self.tokenizer.model_max_length > 0
+                        else False
+                    )
+                    inputs = self.tokenizer(
+                        sentence,
+                        return_tensors=self.framework,
+                        truncation=truncation,
+                        return_special_tokens_mask=True,
+                        return_offsets_mapping=self.tokenizer.is_fast,
+                        is_split_into_words=True,
+                        **tokenizer_params,
+                    )
+                    inputs.pop("overflow_to_sample_mapping", None)
+                    num_chunks = len(inputs["input_ids"])
+
+                    for i in range(num_chunks):
+                        if self.framework == "tf":
+                            raise Exception(
+                                "TensorFlow pipelines are currently not supported."
+                            )
+                        else:
+                            model_inputs = {
+                                k: v[i].unsqueeze(0) for k, v in inputs.items()
+                            }
+                        if offset_mapping is not None:
+                            model_inputs["offset_mapping"] = offset_mapping
+                        model_inputs["sentence"] = sentence if i == 0 else None
+                        model_inputs["is_last"] = i == num_chunks - 1
+
+                        yield model_inputs
+
+            tokenizer = AutoTokenizer.from_pretrained(
+                model_args.tokenizer_name, add_prefix_space=True
+            )
+            tokenized_test_data = test_data.map(tokenize_and_align_labels, batched=True)
+            model = AutoModelForTokenClassification.from_pretrained(
+                model_args.model_name_or_path
+            )
+            pipe = MyTokenClassificationPipeline(
+                model=model,
+                tokenizer=tokenizer,
+                device=0,
+                aggregation_strategy="none",
+            )
+            sentences = list(
+                map(
+                    pipe.tokenizer.convert_ids_to_tokens,
+                    tokenized_test_data["input_ids"],
+                )
+            )
+            true = list(map(process_true_labels, tokenized_test_data["labels"]))
+            predictions = pipe(test_data["tokens"])
+            predicted = []
+            for prediction, sentence in zip(predictions, sentences):
+                predicted.append(process_predicted_labels(prediction, sentence))
         with open(prediction_arguments.prediction_file, "w", encoding="utf-8") as f:
             for i in range(len(sentences)):
                 for piece, t, p in zip(sentences[i], true[i], predicted[i]):
                     f.write(f"{piece}\t{t}\t{p}\n")
                 f.write("\n")
-
